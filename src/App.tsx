b87a95327b939eb6268f3a460de6b1c46c25e73e
maeda <takanori@stroly.jp>
2019-09-08 00:41:28 +0900

    ルーティング対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Container from './Container';
 
 const App: React.FC = () => {
-  return <Container />;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Container} />
+      <Route path="/:channel" component={Container} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
